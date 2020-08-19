e52a894bf15126ba76c60113263ccd08fdf6eab4
MattSzm <mateuszszmal@gmail.com>
2020-08-19 12:46:09 +0200

    added like/comments/share icons

--- src/components/Post/PostContent/PostContent.js
+++ src/components/Post/PostContent/PostContent.js
@@ -1,10 +1,14 @@
 import React from "react";
 import classes from './PostContent.module.css';
+import likeButton from '../../../assets/icons/like.png'
+import commentButton from '../../../assets/icons/comment.png'
+import shareButton from '../../../assets/icons/share.png';
+
 
 const hashtag_formatter = string => {
     return string.split(/((?:^|\s)(?:[@|#][a-z\d-]+))/gi).filter(Boolean).map((v, i) => {
         if(v.includes('#') || v.includes('@')){
-            return <span key={i} style={{color: '#1DA1F2'}}>{v}</span>
+            return <span key={i} className={classes.hashtag}>{v}</span>
         }
         else{
             return <span key={i} >{v}</span>
@@ -18,6 +22,12 @@ const PostContent = (props) => {
         <div className={classes.PostContent}>
             <p className={classes.bolded}>Author nickname <span>&#183; Aug 17</span></p>
             <p>{hashtag_formatter("Goooooodmorning @guyssss! #working in @the studio on new music, what are you doing #today??")}</p>
+
+            <div className={classes.Icons}>
+                <img src={likeButton} />
+                <img src={commentButton} />
+                <img src={shareButton} />
+            </div>
         </div>
     );
 }
